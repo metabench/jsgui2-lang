39ad65779996b7f7d70fbb170d55f34fe7595a63
James Vickers <james@metabench.com>
2016-09-16 04:26:57 +0100

    A neater way of accessing language modules than in jsgui

---
+++ lang.js
@@ -0,0 +1,23 @@
+var jsgui = require('jsgui2-essentials');
+
+var B_Plus_Tree = require('jsgui2-b-plus-tree');
+var Collection = require('jsgui2-collection');
+var Data_Object = require('jsgui2-data-object');
+var Data_Value = require('jsgui2-data-value');
+var Doubly_Linked_List = require('jsgui2-data-value');
+var Evented_Class = require('jsgui2-evented-class');
+var Ordered_KVS = require('jsgui2-ordered-kvs');
+var Ordered_String_List = require('jsgui2-ordered-string-list');
+var Sorted_KVS = require('jsgui2-sorted-kvs');
+
+jsgui.B_Plus_Tree = B_Plus_Tree;
+jsgui.Collection = Collection;
+jsgui.Data_Object = Data_Object;
+jsgui.Data_Value = Data_Value;
+jsgui.Doubly_Linked_List = Doubly_Linked_List;
+jsgui.Evented_Class = Evented_Class;
+jsgui.Ordered_KVS = Ordered_KVS;
+jsgui.Ordered_String_List = Ordered_String_List;
+jsgui.Sorted_KVS = Sorted_KVS;
+
+module.exports = jsgui;
